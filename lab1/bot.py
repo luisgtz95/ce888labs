094b727cd7e579005a777dcec216e11ebf214546
luisgtz95 <lg17474@essex.ac.uk>
2018-01-23 14:17:37 +0000

    lab1

---
+++ lab1/bot.py
@@ -0,0 +1,14 @@
+from sopel import module
+from emo.wdemotions import EmotionDetector
+
+emo = EmotionDetector()
+
+
+@module.rule('')
+def hi(bot, trigger):
+    print((trigger, trigger.nick))
+    #bot.say('Hi, ' + trigger.nick)
+    count = emo.detect_emotion_in_raw(str(trigger)
+    print (count)
+
+
